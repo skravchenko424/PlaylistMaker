07107686ff7c7b63a8e7aa22e96e85accfa485be
Stanislav Kravchenko <67255547+skravchenko424@users.noreply.github.com>
2026-05-25 11:44:54 +0300

    Playlist Maker Homework (#1)
    * Init commit: first layouts
    
    * Buttons show toasts
    
    * Implemented navigation to other screens
    
    * Review fixes
    
    * Fixed vertical margin

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +13,37 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // anon class for the first button
+        val buttonSearch = findViewById<Button>(R.id.search_button)
+        val buttonSearchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val displayIntent = Intent(v?.context, SearchActivity::class.java)
+                startActivity(displayIntent)
+            }
+        }
+        buttonSearch.setOnClickListener(buttonSearchClickListener)
+
+        // lambda for the second button
+        val buttonMedia = findViewById<Button>(R.id.media_library_button)
+        buttonMedia.setOnClickListener {
+            val displayIntent = Intent(this, MediaLibraryActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+        // lambda for the third button
+        val buttonSettings = findViewById<Button>(R.id.settings_button)
+        buttonSettings.setOnClickListener {
+            val displayIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent)
+        }
     }
 }
